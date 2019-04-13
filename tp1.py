9eb0cd2bd6e20d187e8ce3fbdb66cce90b4152e7
maximarin <31014199+maximarin@users.noreply.github.com>
2019-04-13 18:31:54 -0300

    Función para reconocer g2

--- tp1.py
+++ tp1.py
@@ -27,15 +27,13 @@ def DeterminarGrado3(cadena):
     if minusculasAntecedente>=1:
         band = False
         error = lista2[x]
-        texto = "Existe un terminal en el antecedente: nt->t"
+        texto = "Existe un terminal en el antecedente: t->t"
 
     if mayusculasAntecedente>1:
         band = False
         error = lista2[x]
         texto = "Existe mas de un no terminal en el antecedente: NTNT->t"
 
-    if listaDeLista[1]=="lambda":
-        texto="Existe lambda en el consecuente: NT->lambda"
 
     mayusculasconsecuente = len([c for c in listaDeLista[1] if c.isupper()])
     minusculasConsecuente = len([c for c in listaDeLista[1] if c.islower()])
@@ -75,23 +73,61 @@ def DeterminarGrado3(cadena):
 
 
 
-print (DeterminarGrado3(("A:b A\nA:a\nA:A B c\nA:lambda\nB:b")))
+"""print (DeterminarGrado3(("A:b A\nA:a\nA:A B c\nA:lambda\nB:b")))"""
 
 bandera = 0
 """Funcion para determinar si es g2"""
 
-def verificarSiEsG2(Lista):
+def DeterminarGrado2(cadena):
+    error = []
     texto = ""
-    return texto
+    ListaCadena = cadena.split("\n")
+    band = True
+
+    lista2 = []
+    for x1 in range(0, len(ListaCadena)):
+        lista2.append(ListaCadena[x1].split(":"))
+
+    listaDeLista = []
+
+    for x in range(0, len(lista2)):
+        listaDeLista = lista2[x]
+
+    mayusculasAntecedente = len([c for c in listaDeLista[0] if c.isupper()])
+    minusculasAntecedente = len([c for c in listaDeLista[0] if c.islower()])
+
+    if minusculasAntecedente >= 1:
+        band = False
+        error = lista2[x]
+        texto = "Existe un terminal en el antecedente: t ->t"
+
+    if mayusculasAntecedente > 1:
+        band = False
+        error = lista2[x]
+        texto = "Existe mas de un no terminal en el antecedente: NTNT->t"
+
+
+    mayusculasconsecuente = len([c for c in listaDeLista[1] if c.isupper()])
+    minusculasConsecuente = len([c for c in listaDeLista[1] if c.islower()])
+
+
+    if mayusculasconsecuente > 1:
+        band = False
+        error = lista2[x]
+        texto = "Existe mas de un no terminal en el consecuente : NT->NT NT"
+
+    if band:
+        texto = "Es g2 ya que cumple con todas las condiciones: NT->t, NT->NT t, NT->t NT, NT->NT, NT-> NT tttt"
+        return (texto)
+    else:
+        ExpresionErronea = error[0] + "->" + error[1]
+        return (ExpresionErronea, texto)
 
 
 
-"""Funcion para determinar si es g1"""
+print (DeterminarGrado2(("S:C ba A\nA:a\nA:A B c\nA:lambda\naB:S ")))
 
-def verificarSiEsG1(Lista):
-    texto = ""
-    return texto
-
+bandera= 0
 
 
 """Funcion general llamando a las demas (g3,g2,g1)"""
